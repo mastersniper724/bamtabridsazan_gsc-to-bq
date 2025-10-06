f1162fd0bc9279369756ba666bae24be88473514
mastersniper724 <master.sniper724@gmail.com>
2025-10-06 11:04:05 +0330

    Auto commit from script

--- scripts/upload_gsc_enhancements.py
+++ scripts/upload_gsc_enhancements.py
@@ -268,12 +268,11 @@ def upload_to_bq(df):
         return
     try:
         job_config = bigquery.LoadJobConfig(write_disposition="WRITE_APPEND")
-	# مثال تبدیل ستون‌های تاریخ به datetime	
-	for date_col in ["date", "last_crawled", "fetch_date"]:
-	    if date_col in final_df.columns:
+	# مثال تبدیل ستون‌های تاریخ به datetime
+        for date_col in ["date", "last_crawled", "fetch_date"]:
+            if date_col in final_df.columns:
 		final_df[date_col] = pd.to_datetime(final_df[date_col], errors='coerce').dt.date
         
-
         job = bq_client.load_table_from_dataframe(df, f"{PROJECT_ID}.{DATASET_ID}.{TABLE_ID}", job_config=job_config)
         job.result()
         print(f"[INFO] Inserted {len(df)} rows to {PROJECT_ID}.{DATASET_ID}.{TABLE_ID}.")
