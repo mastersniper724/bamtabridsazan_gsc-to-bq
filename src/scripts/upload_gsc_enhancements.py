b10a66552911af53ef2cba54f8446f8b7c69b52e
mastersniper724 <master.sniper724@gmail.com>
2025-10-08 14:31:45 +0330

    Auto commit from script

--- src/scripts/upload_gsc_enhancements.py
+++ src/scripts/upload_gsc_enhancements.py
@@ -31,7 +31,7 @@ PROJECT_ID = "bamtabridsazan"
 DATASET_ID = "seo_reports"
 TABLE_ID = "bamtabridsazan__gsc__raw_enhancements"
 MAPPING_TABLE = f"{PROJECT_ID}.{DATASET_ID}.bamtabridsazan__gsc__searchappearance_enhancement_mapping"
-GITHUB_LOCAL_PATH_DEFAULT = "gsc_enhancements"
+GITHUB_LOCAL_PATH_DEFAULT = "data/raw/search_appearance/gsc_enhancements"
 
 FINAL_COLUMNS = [
     "site",
